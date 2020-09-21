0769f5bc4b3f761f6070f8541f530a3619e7e703
hejie <565703319@qq.com>
2020-09-21 20:44:50 +0800

    [hejie]: object pass

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const result = Object.values(source);
+  let sum = 0;
+  result.forEach(function transAndSum(item) {
+    sum += Number(item);
+  });
+  return sum;
 }
